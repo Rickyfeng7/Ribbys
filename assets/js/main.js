13246dcdf0d0a17f4c5b5baf63d2a6620d981a4d
Ricky Feng <19ricky92@gmail.com>
2020-04-19 18:27:35 -0700

    flickity caroscel

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,37 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
     });
+    $(".button-collapse").sideNav({
+        breakpoint: 1200
+    });
+    // SideNav Scrollbar Initialization
+    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    var ps = new PerfectScrollbar(sideNavScrollbar);
+    // Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+// Carousel
+$('.main-gallery').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+  var flkty = new Flickity( '.main-gallery', {
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
 });
