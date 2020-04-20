42c1538081313cec5a0ae6d24cde2cf8f7a12735
Ricky Feng <19ricky92@gmail.com>
2020-04-20 11:29:07 -0700

    adding logo and icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $(this).toggleClass('active');
@@ -12,7 +38,7 @@ $(document).ready(function () {
     // Get the video
 var video = document.getElementById("myVideo");
 
-// Get the button
+// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
 // Pause and play the video, and change the button text
@@ -36,4 +62,5 @@ $('.main-gallery').flickity({
     cellAlign: 'left',
     contain: true
   });
+
 });
