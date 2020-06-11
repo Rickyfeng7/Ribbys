b14cb1644d0b45cfcc9e2663db81b223c8e9ed5f
Ricky Feng <19ricky92@gmail.com>
2020-06-11 02:19:38 -0700

    adding modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,76 @@
 $(document).ready(function () {
 
-  // Get the modal
-  var modal = document.getElementById("myModal");
-
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks the button, open the modal 
-  btn.onclick = function () {
-    modal.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-
   // Get the video
-  var video = document.getElementById("myVideo");
+  // var video = document.getElementById("myVideo");
 
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
+  // timer
 
-  // Pause and play the video, and change the button text
-  function myFunction() {
-    if (video.paused) {
-      video.play();
-      btn.innerHTML = "Pause";
-    } else {
-      video.pause();
-      btn.innerHTML = "Play";
+  function countdown(endDate) {
+    var days, hours, minutes, seconds;
+
+    endDate = new Date(endDate).getTime();
+
+    if (isNaN(endDate)) {
+      return;
+    }
+
+    setInterval(calculate, 1000);
+
+    function calculate() {
+      var startDate = new Date().getTime();
+
+      var timeRemaining = parseInt((endDate - startDate) / 1000);
+
+      if (timeRemaining >= 0) {
+        days = parseInt(timeRemaining / 86400);
+        timeRemaining = (timeRemaining % 86400);
+
+        hours = parseInt(timeRemaining / 3600);
+        timeRemaining = (timeRemaining % 3600);
+
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining = (timeRemaining % 60);
+
+        seconds = parseInt(timeRemaining);
+
+        document.getElementById("days").innerHTML = parseInt(days, 10);
+        document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
+        document.getElementById("minutes").innerHTML = minutes < 10 ? "0" + minutes : minutes;
+        document.getElementById("seconds").innerHTML = seconds < 10 ? "0" + seconds : seconds;
+      } else {
+        return;
+      }
     }
   }
-  // Carousel
-  // $('.main-gallery').flickity({
-  //     // options
-  //     cellAlign: 'left',
-  //     contain: true
-  //   });
-  //   var flkty = new Flickity( '.main-gallery', {
-  //     // options
-  //     cellAlign: 'left',
-  //     contain: true
-  //   });
-  function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
 
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
+  (function () {
+    countdown('06/19/2020 12:00:00 AM');
+  }());
+  //   function openPage(pageName, elmnt, color) {
+  //     // Hide all elements with class="tabcontent" by default */
+  //     var i, tabcontent, tablinks; tabcontent = document.getElementsByClassName("tabcontent");
+  //     for (i = 0; i < tabcontent.length; i++) {
+  //       tabcontent[i].style.display = "none";
+  //     }
 
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
+  //     // Remove the background color of all tablinks/buttons
+  //     tablinks = document.getElementsByClassName("tablink");
+  //     for (i = 0; i < tablinks.length; i++) {
+  //       tablinks[i].style.backgroundColor = "";
+  //     }
 
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-  }
+  //     // Show the specific tab content
+  //     document.getElementById(pageName).style.display = "block";
 
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+  //     // Add the specific color to the button used to open the tab content
+  //     elmnt.style.backgroundColor = color;
+  //   }
 
-  
+
+  $("#myModal").modal('show');
+
+  //   // Get the element with id="defaultOpen" and click on it
+  //   document.getElementById("defaultOpen").click();
+
+
 });
